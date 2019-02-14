5a212f1fb2fb6d793fdad5c90b23a2d525e6709e
Alex Freire <alexfreirespinola@gmail.com>
2019-02-14 01:10:31 -0300

    add new features

--- src/prisma.js
+++ src/prisma.js
@@ -1,11 +1,17 @@
 import { Prisma } from 'prisma-binding';
+// prisma binding vai usar o typeDefs que o graphql-cli encontrou no endpoint que está rodando a aplicação do playground
+// e trouxe para a aplicação. o arquivo .graphqlconfig é responsável por configurar o graphql-cli
+//
 
 const prisma = new Prisma({
-    typeDefs: './generated/prisma.graphql',
+    typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466',
-
 })
 
-prisma.query.users(null, '{ id name email posts { title } }').then((data) => {
-    console.log(data)
+// prisma.query.users(null, '{ id name email posts { id title } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 4))
+// })
+
+prisma.query.comments(null , '{id text author {id name}}').then(data => {
+    console.log(JSON.stringify(data, undefined, 4))
 })
