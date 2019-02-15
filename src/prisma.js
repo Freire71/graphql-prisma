b221cca073dc343029227c8ffaf8055b16be36a5
Alex Freire <alexfreirespinola@gmail.com>
2019-02-15 19:27:31 -0300

    commit all

--- src/prisma.js
+++ src/prisma.js
@@ -12,6 +12,20 @@ const prisma = new Prisma({
 //     console.log(JSON.stringify(data, undefined, 4))
 // })
 
-prisma.query.comments(null , '{id text author {id name}}').then(data => {
+// prisma.query.comments(null , '{id text author {id name}}').then(data => {
+//     console.log(JSON.stringify(data, undefined, 4))
+// })
+prisma.mutation.createPost({
+    data: {
+        title: 'um titulo bacana',
+        body: 'um body bacana',
+        published: true,
+        author: {
+            connect: {
+                id: 'cjs6gw8na004p07590okq584l'
+            }
+        }
+    }
+}, '{ id title author { id name } }').then(data => {
     console.log(JSON.stringify(data, undefined, 4))
 })
